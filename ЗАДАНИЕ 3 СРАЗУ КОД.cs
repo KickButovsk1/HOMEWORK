5cd31f6b3dba19883f4854a2fa2d1b9afad0d82b
KickButovsk1 <144268614+KickButovsk1@users.noreply.github.com>
2023-09-11 01:32:39 +0500

    Add files via upload

---
+++ ЗАДАНИЕ 3 СРАЗУ КОД.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = Console.ReadLine();
+            int dva, tri, ch;
+            bool kishlak = Int32.TryParse(first, out dva);
+            if (kishlak == true)
+
+            {
+                tri = dva + 1;
+                ch = dva - 1;
+                Console.WriteLine($"The next number for the number {dva} is {tri}." + $"The previous number for the number {dva} is {ch}.");
+            }
+            else
+            {
+                Console.WriteLine("Это не число.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
